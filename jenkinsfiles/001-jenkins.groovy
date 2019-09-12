a58e0a4fa44f66e544bcfc5a07df9376de310138
shashankreed <shashankreed@gmail.com>
2019-09-12 12:58:45 +0530

    Jenkins slack builds

--- jenkinsfiles/001-jenkins.groovy
+++ jenkinsfiles/001-jenkins.groovy
@@ -36,4 +36,18 @@ stage('build') {
 }
 }
   }
+    configure { project ->
+    project / publishers << 'jenkins.plugins.slack.SlackNotifier' {
+      baseUrl("https://whatever.slack.com/services/hooks/jenkins-ci/")
+      room("#room")
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(true)
+      notifySuccess(true)
+      notifyUnstable(true)
+      notifyRegression(true)
+      notifyBackToNormal(true)
+      startNotification(true)
+    }
+  }
 }

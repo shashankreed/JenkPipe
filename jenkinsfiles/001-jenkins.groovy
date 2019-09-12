e4aee7d233fe45c1e93b102d40256847b6aee473
shashankreed <shashankreed@gmail.com>
2019-09-13 04:16:49 +0530

    job update

--- jenkinsfiles/001-jenkins.groovy
+++ jenkinsfiles/001-jenkins.groovy
@@ -5,10 +5,11 @@ stage('build1') {
 	git url: "https://bitbucket.org/tekion/tekionbuild.git", credentialsId: 'smganesha', branch: 'master'
 }
 }
+/*
 stage('build2') {
     steps {
       git url:"https://bitbucket.org/tekion/tfees.git", credentialsId: 'smganesha', branch: 'b7U'
-//        checkout([$class: 'GitSCM', userRemoteConfigs: [[credentialsId: 'smganesha', url: 'https://bitbucket.org/tekion/tfees.git']], branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'tfees']], submoduleCfg: []])
+        checkout([$class: 'GitSCM', userRemoteConfigs: [[credentialsId: 'smganesha', url: 'https://bitbucket.org/tekion/tfees.git']], branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'tfees']], submoduleCfg: []])
     withEnv(['VAR1=VALUE ONE',
 	     'AWS_CLUSTER_NAME=ecs-devalpha-cluster',
 	     'SERVICE_NAME=tfees_ecsdev',
@@ -22,4 +23,5 @@ stage('build2') {
     }
 }
 }
+*/
 }

2e6163d0c2d36c975f5f55ebdba6312b88224e3a
shashankreed <shashankreed@gmail.com>
2019-09-12 12:38:40 +0530

    Jenkins buil mods

--- jenkinsfiles/001-jenkins.groovy
+++ jenkinsfiles/001-jenkins.groovy
@@ -20,6 +20,18 @@ pipeline {
         }
       }
     }
-
+stage('build') {
+    def someGroovyVar = 'Hello world'
+    withEnv(['VAR1=VALUE ONE',
+             "VAR2=${someGroovyVar}"
+	     'AWS_CLUSTER_NAME=ecs-devalpha-cluster',
+	     'SERVICE_NAME=tfees_ecsdev',
+	     'REGION_NAME=us-west-1',
+	     'BRANCH_NAME=release-1.9'
+            ]) {
+        def result = sh(script: 'echo $VAR1; echo $AWS_CLUSTER_NAME; echo $VAR2', returnStdout: true)
+        echo result
+    }
+}
   }
 }

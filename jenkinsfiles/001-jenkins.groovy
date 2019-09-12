50094314878438f6eba8c0384cd0893998fadcd7
shashankreed <shashankreed@gmail.com>
2019-09-12 15:52:58 +0530

    Jenkins git builds

--- jenkinsfiles/001-jenkins.groovy
+++ jenkinsfiles/001-jenkins.groovy
@@ -21,7 +21,7 @@ pipeline {
     }
 stage('build') {
     steps {
-      git Url:"https://bitbucket.org/tekion/tappointment/"
+      git url:"https://bitbucket.org/tekion/tappointment/"
     withEnv(['VAR1=VALUE ONE',
 	     'AWS_CLUSTER_NAME=ecs-devalpha-cluster',
 	     'SERVICE_NAME=tfees_ecsdev',

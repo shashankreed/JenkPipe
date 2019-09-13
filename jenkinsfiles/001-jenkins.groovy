bfe73c32aa687e22e12e6f886edca5eea8ef83f4
shashankreed <shashankreed@gmail.com>
2019-09-13 05:48:13 +0530

    job update

---
+++ jenkinsfiles/001-jenkins.groovy
@@ -0,0 +1,42 @@
+pipeline {
+  agent any
+stages {
+stage('build1') {
+	steps{
+	git url: "https://bitbucket.org/tekion/tekionbuild.git", credentialsId: 'smganesha', branch: 'master'
+    checkout([$class: 'GitSCM', userRemoteConfigs: [[credentialsId: 'smganesha', url: 'https://bitbucket.org/tekion/tekionbuild.git']], branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'tekionbuild']], submoduleCfg: []])
+
+}
+}
+stage('build2') {
+    steps {
+     ///  git url:"https://bitbucket.org/tekion/tfees.git", credentialsId: 'smganesha', branch: 'b7U'
+checkout([$class: 'GitSCM', userRemoteConfigs: [[credentialsId: 'smganesha', url: 'https://bitbucket.org/tekion/tfees.git']], branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'tfees23']], submoduleCfg: []])
+    withEnv(['VAR1=VALUE ONE',
+	     'AWS_CLUSTER_NAME=ecs-devalpha-cluster',
+	     'SERVICE_NAME=tfees_ecsdev',
+	     'REGION_NAME=us-west-1',
+	     'BRANCH_NAME=release-1.9'
+            ]) {
+        sh '''
+#!/bin/bash
+cp tekionbuild/scripts/tekion-common-build.sh tappointment
+cp tekionbuild/scripts/nestedDependency.sh tappointment
+cd tappointment
+#bash -ex nestedDependency.sh
+
+cd vendor/bitbucket.org/tekion
+rm -rf tappointment
+#rm -rf tbaas
+#git clone -b release-1.8.1 https://bitbucket.org/tekion/tbaas.git
+#rm -rf tscrittypes
+#git clone -b doubleOptIn https://bitbucket.org/tekion/tscrittypes.git
+cd ../../..
+
+#bash -ex tekion-common-build.sh
+	'''
+    }
+}
+}
+}
+}
